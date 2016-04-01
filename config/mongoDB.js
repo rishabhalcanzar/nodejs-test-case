6827953692049c862c575da61bf1ca3aac70a95e
Asspl <abhishek.bajpai@alcanzarsoft.com>
2016-04-01 19:40:54 +0530

    reverting localhost

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -3,4 +3,14 @@
 var mongoose = require( 'mongoose' );
 var config = require( './index.js' );
 
-mongoose.connect( config.mongoURI );
+// mongoose.connect( config.mongoURI );
+var uri = process.env.MONGOLAB_URI || config.mongoURI || 'mongodb://127.0.0.1/testdb'; 
+console.log('CONNECTING 2 ====> ', uri);
+
+mongoose.connect(uri, {}, function(err, db){
+	if(err) {
+	  console.log('Connection Error ::: ', err);
+	} else {
+	  console.log('Successfully Connected!');
+	}
+});
